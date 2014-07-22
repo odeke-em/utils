8a4abdf34fa001301524c80359a1b772799acef3
Emmanuel Odeke <odeke@ualberta.ca>
2014-07-22 00:08:48 -0600

    More mods

--- dataStructures/hashmap/DMain.c
+++ dataStructures/hashmap/DMain.c
@@ -16,9 +16,17 @@ int main(int argc, char **argv) {
 
     char *fDupd = strdup(__func__);
     ULInt h = pjwCharHash(fDupd);
-    dm = pushDMap(dm, (void *)fDupd, h, 1);
+    char *cast = (char *)dm;
+
+    ULInt ownH = pjwCharHash(cast);
+    void *prevEvac = putDMap(dm, fDupd, h, 1);
+    char *ft = __FILE__;
+    printf("evac: %s fDup: %s h: %ld\n", (char *)prevEvac, fDupd, ownH);
+    prevEvac = putDMap(dm, ft, pjwCharHash(ft), 0);
+    printf("pE: %p\n", prevEvac);
 
     void *retr = getDMap(dm, h);
+    printf("retr: %p\n", retr);
     assert((retr != NULL));
     printf("dn:data %s\n", (char *)retr);
     printf("dmSize: %ld\n", dm->size);
@@ -26,10 +34,10 @@ int main(int argc, char **argv) {
     void *evac = NULL;
     dm = popDMap(dm, h, (const void **)&evac);
     printf("dmSize: %ld evacuated: %s\n", dm->size, (char *)evac);
-    free(evac);
 
+    evac = NULL;
     dm = popDMap(dm, h, (const void **)&evac);
-    printf("dmSize: %ld\n", dm->size);
+    printf("dmSize: %ld evacuated: %s\n", dm->size, (char *)evac);
     dm = destroyDMap(dm);
 
     return 0;
