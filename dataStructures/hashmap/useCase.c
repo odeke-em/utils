7771f697fe2922a8f6272a22522e606ae48b5a68
Emmanuel Odeke <odeke@ualberta.ca>
2014-07-28 21:57:00 -0600

    Took out middleman in python

--- dataStructures/hashmap/useCase.c
+++ dataStructures/hashmap/useCase.c
@@ -11,35 +11,42 @@ int main(int argc, char *argv[]) {
     // Goal here is given an arbitrary stream of integers,
     // take out duplicates however, maintain the set order
     DMap *dm = NULL;
-
-    FILE *ifp = stdin;
-
-    ULInt i_mBuf=0;
-    unsigned int prevRead=0, MBUF_BLK_SZ=sysconf(_SC_PAGE_SIZE);
-    unsigned long long int res=0;
-
-    char c;
-    struct MBuf *mb = freshMBuf(MBUF_BLK_SZ);
-    while ((c = getc(ifp)) != EOF) {
-        if (isdigit(c)) {
-            res = (res * 10) + (c - '0');
-            prevRead = 1;
+    unsigned long long int n = 1000000, i, rNum, MAX_NUM = 578918283839281739, MIN_NUM=0;
+    if (argc >= 2) {
+        if (sscanf(argv[1], "%lld", &n) != 1)
+            n = 1000000;
+        if (argc >= 3) {
+            if (sscanf(argv[2], "%lld", &MAX_NUM) != 1)
+                MAX_NUM = 78919127392918;
+            if (argc >= 4) {
+                if (sscanf(argv[3], "%lld", &MIN_NUM) != 1)
+                    MIN_NUM = 1;
+            }
         }
-        else if (prevRead) {
-            prevRead = 0;
+    }
+
+    printf("argc: %d c: %lld min: %lld max: %lld\n", argc, n, MIN_NUM, MAX_NUM);
+
+    unsigned long long int i_mBuf=0;
+    unsigned int prevRead=0, MBUF_BLK_SZ=sysconf(_SC_PAGE_SIZE);
+
+    struct MBuf *mb = freshMBuf(MBUF_BLK_SZ);
+    for (i=0; i < n; ++i) {
             if (i_mBuf >= mb->size) {
+                printf("%lld/%lld\r", i_mBuf, n);
                 mb = resizeMBuf(mb, mb->size + MBUF_BLK_SZ);
                 _fillBufWithContent(mb->buf + i_mBuf, mb->buf+mb->size, twoCharFill);
             }
 
-            dm = pushDMap(dm, *(mb->buf + i_mBuf), res, 1);
+            while ((rNum = rand() * MAX_NUM) < MIN_NUM && rNum >= MAX_NUM) {
+                printf("ici\n");
+            }
+
+            dm = pushDMap(dm, *(mb->buf + i_mBuf), rNum, 1);
             ++i_mBuf;
-            res = 0;
-            printf("#%ld read\n", i_mBuf);
-        }
     }
 
-    printf("dmSize: %ld\n", getSize(dm));
+    printf("\ndmSize: %ld\n", getSize(dm));
     free(mb); // Note: Won't be invoking destroyMBuf since memory in buf is freed during destroyDMap
     dm = destroyDMap(dm);
 };
