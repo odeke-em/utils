896460266183885b25d9574f468c74386dc0df74
Emmanuel Odeke <emm.odeke@gmail.com>
2015-03-16 19:45:04 -0600

    collab_summarizer: tac first then use only date and msg

--- collab_summarizer.sh
+++ collab_summarizer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Preparing collaborated code for assignment submission
+
 out_file=summary.log
-git log | awk 'BEGIN {i=0} { if ($0 ~ /Author/) { i+=1; print "\n+ Change "i } else { print "\t"$0 }}' > $out_file && echo -e "\033[92mWrote summary to $out_file\033[00m" || echo -e "\033[91mFailed to write changes to $out_file\033[00m"
+git log | tac | awk '{ if ($0 ~ /^commit/) { print "\n**\n" } else if ($0 !~ /Author/) { print $0 }}' > $out_file && echo -e "\033[92mWrote summary to $out_file\033[00m" || echo -e "\033[91mFailed to write changes to $out_file\033[00m"
